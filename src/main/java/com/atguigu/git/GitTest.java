e197ca8af37c8571eeea288e0d758218b76250a1
zhangjian <zhangjian@atguigu.com>
2022-04-12 14:10:35 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("哈哈一定把git好");
+        System.out.println("啧啧终于学git了");
     }
 }
