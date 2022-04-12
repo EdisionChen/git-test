9e38c2ef565c966ae26e18cde439b9b196ef0e9f
zhangjian <zhangjian@atguigu.com>
2022-04-12 10:55:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author张瑊
+ * @version1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("哈哈一定把git好");
+    }
+}
