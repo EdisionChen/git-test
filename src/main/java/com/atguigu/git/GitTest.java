a791db0ae99144e187df2ae1f0cdc43d431ce6bd
EdisionChen <103424378+EdisionChen@users.noreply.github.com>
2022-04-12 14:40:03 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("哈哈一定把git好");
         System.out.println("啧啧终于学git了");
         System.out.println("哈哈这个github网站好慢呀");
+        System.out.println("pull代码演示");
     }
 }
